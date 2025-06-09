13bc6ee65df2bcab7418ea0878f255fc6cf6619e
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-06-09 06:56:06 +0100

    other scientific  functions

--- Scientific calculator/sci_calc.js
+++ Scientific calculator/sci_calc.js
@@ -44,3 +44,11 @@ const tan = () => {display.value = Math.tan(display.value)};
 const sinh = () => {display.value = Math.asin(display.value)};
 const cosh = () => {display.value = Math.acos(display.value)};
 const tanh = () => {display.value = Math.atan(display.value)};
+const exp = () => {display.value = Math.exp(display.value)};
+const ln = () => {display.value = Math.log(display.value)};
+const l0g = () => {display.value = Math.log10(display.value)};
+const cubert = () => {display.value = Math.cbrt(display.value)};
+const squarert = () => {display.value = Math.sqrt(display.value)};
+const square = () => {display.value = Math.pow(2)};
+const cube = () => {display.value = Math.pow(3)};
+const abs = () => {display.value = Math.abs()}
