ffc2b0eca95d3d6e1030a116b4ccab574032b43f
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-06-10 17:57:16 +0100

    function for if guess is not a number or out of range

--- Number guessing game/guess.js
+++ Number guessing game/guess.js
@@ -12,3 +12,32 @@ const maxNum = 20;
 const compNum = Math.floor(Math.random() * (maxNum - (minNum - 1))) + minNum;
 
 let chances = 10;
+chance.innerText = chances;
+
+// Code to initiate and play game
+    const playGame = () => {
+       
+        let  guess = Number(playerNum.value);
+   
+        if(isNaN(guess)){
+          wrongAns.innerText = "Please enter a valid number";
+          chances--;
+          chance.innerText = chances;
+        }
+
+        else if (guess < minNum){
+          wrongAns.innerText = "Your Number is less than range";
+          chances--;
+          chance.innerText = chances;
+        }
+
+        else if (guess > maxNum){
+          wrongAns.innerText = "Your Number is greater than range";
+          chances--;
+          chance.innerText = chances;
+        }
+
+        else {
+        
+      }
+    }
