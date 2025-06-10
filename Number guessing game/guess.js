0ac715cc69d2553d83215dede1197f925ef3a7b8
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-06-10 17:49:04 +0100

    Getting the HTML documents

---
+++ Number guessing game/guess.js
@@ -0,0 +1,7 @@
+
+// Getting the HTML elements
+const playerNum = document.getElementById("playernum");
+const wrongAns = document.getElementById("wrongans");
+const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit");
+const chance = document.getElementById("chances");
