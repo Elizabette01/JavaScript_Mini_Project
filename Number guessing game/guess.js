6dea82f13ed977d7e8beb820922faf4898ebc240
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-06-10 17:52:12 +0100

    getting html element and random number

--- Number guessing game/guess.js
+++ Number guessing game/guess.js
@@ -5,3 +5,10 @@ const wrongAns = document.getElementById("wrongans");
 const result = document.getElementById("result");
 const submitBtn = document.getElementById("submit");
 const chance = document.getElementById("chances");
+
+// Function to get a random number as computer value from a given range
+const minNum = 1;
+const maxNum = 20;
+const compNum = Math.floor(Math.random() * (maxNum - (minNum - 1))) + minNum;
+
+let chances = 10;
