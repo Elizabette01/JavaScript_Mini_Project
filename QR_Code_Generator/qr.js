dbcf9eedb28403f89d40648a9f194b9f28e59b1d
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-06-12 14:57:54 +0100

    getting all elements to be manipulated

---
+++ QR_Code_Generator/qr.js
@@ -0,0 +1,7 @@
+const inputTxt = document.getElementById("input_txt");
+const sizes = document.getElementById("sizes");
+const generateBtn = document.getElementById("generate_btn");
+const downloadBtn = document.getElementById("download_btn");
+const qrContainer = document.getElementById("qr_container");
+const BGColor = document.getElementById("bg_color");
+const FGColor = document.getElementById("fg_color");
